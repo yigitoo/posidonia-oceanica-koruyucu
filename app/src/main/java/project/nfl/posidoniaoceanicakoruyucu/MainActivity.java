2bc3c02e088764cf52b9d8a42df78bd35ec5f0f4
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-06-18 01:47:09 +0300

    .

---
+++ app/src/main/java/project/nfl/posidoniaoceanicakoruyucu/MainActivity.java
@@ -0,0 +1,73 @@
+package project.nfl.posidoniaoceanicakoruyucu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView mWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webview);
+        mWebView.setWebViewClient(new myWebViewclient()); // to handle URL redirects in the app
+        mWebView.getSettings().setJavaScriptEnabled(true); // to enable JavaScript on web pages
+        mWebView.getSettings().setGeolocationEnabled(true); // to enable GPS location on web pages
+        mWebView.getSettings().setDisplayZoomControls(false);
+        mWebView.loadUrl("https://f003-78-185-171-64.ngrok-free.app");
+    }
+
+    public class myWebViewclient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+
+            return true;
+        }
+
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+
+            final String urls = url;
+            if (urls.contains("mailto") || urls.contains("whatsapp") || urls.contains("tel") || urls.contains("sms") || urls.contains("facebook") || urls.contains("truecaller") || urls.contains("twiter")) {
+                mWebView.stopLoading();
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(urls));
+                startActivity(i);
+
+
+            }
+
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
