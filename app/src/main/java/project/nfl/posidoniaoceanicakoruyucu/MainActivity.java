e6bc0c8edccac90609f2e433799692ac95abac58
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-06-19 17:05:09 +0300

    change AndroidManifest (gps) and changed java code

--- app/src/main/java/project/nfl/posidoniaoceanicakoruyucu/MainActivity.java
+++ app/src/main/java/project/nfl/posidoniaoceanicakoruyucu/MainActivity.java
@@ -2,6 +2,7 @@ package project.nfl.posidoniaoceanicakoruyucu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -12,12 +13,14 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     WebView mWebView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = findViewById(R.id.webview);
+        mWebView = findViewById(R.id.webView);
         mWebView.setWebViewClient(new myWebViewclient()); // to handle URL redirects in the app
         mWebView.getSettings().setJavaScriptEnabled(true); // to enable JavaScript on web pages
         mWebView.getSettings().setGeolocationEnabled(true); // to enable GPS location on web pages
@@ -46,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                     return true;
             }
-
         }
         return super.onKeyDown(keyCode, event);
     }
@@ -61,11 +63,9 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
-
 
             final String urls = url;
             if (urls.contains("mailto") || urls.contains("whatsapp") || urls.contains("tel") || urls.contains("sms") || urls.contains("facebook") || urls.contains("truecaller") || urls.contains("twitter")) {
@@ -74,20 +74,12 @@ public class MainActivity extends AppCompatActivity {
                 i.setAction(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(urls));
                 startActivity(i);
-
-
             }
-
-
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-
         }
-
-
     }
-
 }
